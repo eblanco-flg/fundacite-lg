e69b58a68ab8f02881304e926e4d48a08b3716df
blancoek <blancoek@gmail.com>
2025-01-14 15:08:38 -0400

    Moved canvas

--- ai-demo/people/script.js
+++ ai-demo/people/script.js
@@ -17,7 +17,8 @@ function startVideo() {
 
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
+  video.insertAdjacentElement('afterend', canvas);
+  //document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
